9372bbc9e12c017995446f4d1832e5b833cd17a4
Tyler Smith <tyler.savon.smith@gmail.com>
2024-08-10 03:49:26 -0600

    add csv database file for storing emails

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import csv
+
 app = Flask(__name__)
 
 
@@ -12,11 +14,28 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email},{subject},{message}')
+        
+        
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as file:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        writer = csv.writer(file, lineterminator="\n")
+        writer.writerow([email,subject,message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_csv(data)
         return redirect('thankyou.html')
     else:
         return 'something went wrong'
